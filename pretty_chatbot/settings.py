0aef0f3fdc5d00f8072ec4f0d81725ad597d7a2b
tinh <ngongtopro>
2025-08-12 11:20:51 +0700

    fix ci

--- pretty_chatbot/settings.py
+++ pretty_chatbot/settings.py
@@ -65,10 +65,10 @@ DATABASES = {
          'ENGINE': 'django.db.backends.{}'.format(
              os.getenv('DATABASE_ENGINE', 'sqlite3')
          ),
-         'NAME': os.getenv('DATABASE_NAME', 'polls'),
-         'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
-         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
-         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+         'NAME': os.getenv('DATABASE_NAME', 'postgres'),
+         'USER': os.getenv('DATABASE_USERNAME', 'postgres'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD', ''),
+         'HOST': os.getenv('DATABASE_HOST', 'localhost'),
          'PORT': os.getenv('DATABASE_PORT', '5432'),
      }
  }
